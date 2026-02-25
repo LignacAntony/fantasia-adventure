3dfe4e53ea48f74a6ec23ea663d759f324d7d79f
TimosLeBoss <tatibatchi15@hotmail.com>
2026-02-25 23:04:54 +0100

    remove

--- frontend/src/00_infra/socket/page.tsx
+++ frontend/src/00_infra/socket/page.tsx
@@ -2,7 +2,7 @@
 
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL ?? "http://localhost:3001";
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL;
 
 export const socket: Socket = io(SOCKET_URL, {
     autoConnect: false,
