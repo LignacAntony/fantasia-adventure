15dda03aaa768a70faeb4ef7ebf92590a18ec575
Oceloott <baptiste.ballesteros@ecole-decode.fr>
2026-03-01 03:02:25 +0100

    deploy

--- frontend/src/00_infra/socket/page.tsx
+++ frontend/src/00_infra/socket/page.tsx
@@ -2,4 +2,6 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001");
+const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3001";
+
+export const socket = io(apiUrl);
