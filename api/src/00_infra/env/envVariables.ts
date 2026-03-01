e97d85b1419abc8fc55cafc688e1b672c01f0d3e
Oceloott <60733546+Oceloott@users.noreply.github.com>
2026-03-01 14:29:55 +0100

    Baptisteballesteros/fan 112 hebergement du projet su railway (#8)
    * deploy
    
    * deploy

--- api/src/00_infra/env/envVariables.ts
+++ api/src/00_infra/env/envVariables.ts
@@ -4,7 +4,7 @@ import z from "zod";
 dotenv.config({ path: ".env.local" });
 
 const EnvVariablesSchema = z.object({
-  FRONTEND_URL: z.string(),
+  FRONTEND_URL: z.string().default("http://localhost:3000"),
 });
 export const envVariables = EnvVariablesSchema.parse({
   FRONTEND_URL: process.env.FRONTEND_URL,
